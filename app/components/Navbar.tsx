16146a4a9da1abd49286be46111b0afa93e0f48a
NGUYEN VAN QUY  Assistant/(Part) Sales & CS <nquy50771@lgcns.com>
2025-10-15 16:10:12 +0700

    authen basic

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,4 +1,8 @@
+"use client"
+import { login } from "../lib/apiAuth";
+
 export default function Navbar() {
+  const handleLogin = () => login();
   return (
     <nav className="fixed top-0 w-full bg-white shadow z-50">
       <ul className="flex justify-center space-x-6 p-4">
@@ -6,6 +10,7 @@ export default function Navbar() {
         <li className="hover:text-blue-600 duration-300"><a href="#about" className="hover-underline text-2xl font-semibold">Giới thiệu</a></li>
         <li className="hover:text-blue-600 duration-300"><a href="#contact" className="hover-underline text-2xl font-semibold">Liên hệ</a></li>
       </ul>
+      <button onClick={handleLogin}>Login Admin</button>
     </nav>
   );
 }
---
+++ app/lib/apiAuth.ts
@@ -0,0 +1,37 @@
+import { signInWithPopup, signOut } from "firebase/auth";
+import {auth, provider} from "./firebase";
+
+export const  login = async () => {
+    try {
+        const res = await signInWithPopup(auth, provider);
+        const user = res.user;
+        const allowedEmails = [
+            "nquy50771@gmail.com",
+            "phamthanhnhan2460@gmail.com",
+        ];
+        
+        if (user.email && allowedEmails.includes(user.email)) {
+            window.location.href = "/admin"
+            return user;
+        } else {
+            await signOut(auth);
+            alert("Email của bạn không có quyền truy cập hệ thống!");
+            window.location.href = "/"
+            return null;
+        }
+    } catch (error) {
+        console.error("Lỗi đăng nhập:", error);
+        alert("Đăng nhập thất bại!");
+        return null;
+    }
+}
+export const logout = async () => {
+    try {
+        await signOut(auth);
+        window.location.href = "/"
+    } catch (error) {
+        console.error("Lỗi đăng nhập:", error);
+        alert("Đăng nhập thất bại!");
+        return null;
+    }
+};
