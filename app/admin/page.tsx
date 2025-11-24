b0efb5518c9dd4972ba09ae9a6e5520754e6cb8a
Nguyen Van Quy <nquy50771@gamil.com>
2025-11-24 22:40:00 +0700

    add function + activities for clb basic

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,14 +1,86 @@
 "use client"
+import type { UploadChangeParam } from "antd/es/upload";
+import type { UploadFile } from "antd/es/upload/interface";
+import { FormDataType } from "../types/type";
 import { useRouter } from "next/navigation";
 import Navbar from "../components/Navbar";
 import { getUserCookie } from "../lib/cookies";
 import { useEffect, useState } from "react";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../lib/firebase";
+import { Modal, Button, Upload, Input, message } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
 
 
 export default function Admin() {
     const router = useRouter();
-    const [nameUser, setNameUser] = useState();
-    
+    const [nameUser, setNameUser] = useState<string | undefined>("");
+    const [previewOpen, setPreviewOpen] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [formData, setFormData] = useState<FormDataType>({
+        title: "",
+        description: "",
+        image: null,
+    });
+
+
+    // function convert file → Base64
+    const toBase64 = (file: File): Promise<string> =>
+        new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result as string);
+        reader.onerror = (err) => reject(err);
+    });
+
+      // Xử lý upload của Antd
+    const handleUploadChange = async (info: UploadChangeParam<UploadFile>) => {
+    const { file, fileList } = info;
+
+    // Nếu người dùng xóa ảnh
+    if (fileList.length === 0) {
+        setFormData(prev => ({ ...prev, image: null }));
+        return;
+    }
+
+    // Chỉ xử lý file mới nhất (vì maxCount=1)
+    const latestFile = fileList[fileList.length - 1];
+    if (latestFile.originFileObj) {
+        try {
+            const base64 = await toBase64(latestFile.originFileObj);
+            // Đảm bảo set đúng, và có thể log để kiểm tra
+            console.log("Base64 đã convert:", base64.slice(0, 50) + "...");
+            
+            setFormData(prev => ({ ...prev, image: base64 }));
+            message.success("Đã thêm ảnh thành công!");
+        } catch (err) {
+            message.error("Lỗi convert ảnh!");
+            console.error(err);
+        }
+    }
+};
+
+
+    // Xử lý lưu vào Firestore
+    const handleConfirm = async () => {
+        if (!formData.title.trim() || !formData.description.trim() || !formData.image) {
+            message.error("Bạn phải nhập Title, Description và Ảnh!");
+            return;
+        }
+            setLoading(true);
+        try {
+            await addDoc(collection(db, "contents_single"), {...formData, createdAt: new Date().toISOString()});
+            Modal.success({title: "Thành công!", content: "Dữ liệu đã được lưu vào Firestore."});
+            // Reset
+            setFormData({title: "", description: "", image: null});
+        } catch (err: unknown) {
+            const errorMessage = err instanceof Error ? err.message : String(err);
+            Modal.error({title: "Lỗi!", content: errorMessage});
+        }
+
+        setLoading(false);
+        setPreviewOpen(false);
+    };
     useEffect(() => {
         let isMouted = true;
         const fetchData = async () => {
@@ -30,8 +102,116 @@ export default function Admin() {
     return (
         <div className="w-full min-h-screen">
             <Navbar/>
-            <div className="w-full h-full mt-64"></div>
-            <h1 className="text-3xl font-bold mb-4">Xin chào, {nameUser}</h1>
+            <div className="w-full h-full px-10 sm:px-16 md:px-20 py-5 lg:py-10 mt-[88px]">
+                <h1 className="text-xl md:text-2xl lg:text-3xl font-bold mb-4">Xin chào, {nameUser}</h1>
+                <div className="mx-auto p-5 bg-white flex flex-col gap-5">
+                    <h2 className="text-lg md:text-xl lg:text-2xl font-semibold">Thêm bài viết về hoạt động mới của lớp</h2>
+                    {/* Title */}
+                    <div className="border-b border-b-gray-200 py-3 flex flex-col items-start gap-3">
+                        <label htmlFor="input-title" className="font-medium">Tiêu đề</label>
+                        <Input
+                            id="input-title"
+                            placeholder="Nhập tiêu đề của bài viết"
+                            value={formData.title}
+                            onChange={(e) =>
+                            setFormData((prev) => ({ ...prev, title: e.target.value }))
+                            }
+                        />
+                    </div>
+
+                    {/* Description */}
+                    <div className="border-b border-b-gray-200 py-3 flex flex-col items-start gap-3">
+                        <label htmlFor="input-description" className="font-medium">Mô tả chi tiết</label>
+                        <Input.TextArea
+                            id="input-description"
+                            placeholder="Nhập nội dung của bài viết"
+                            rows={4}
+                            value={formData.description}
+                            onChange={(e) =>
+                            setFormData((prev) => ({ ...prev, description: e.target.value }))
+                            }
+                        />
+                    </div>
+
+                    {/* Upload Image */}
+                    <div className="py-3 flex flex-col items-start gap-3">
+                        <label htmlFor="input-uploadimg" className="font-medium">Tải lên ảnh</label>
+                        <Upload
+                            id="input-uploadimg"
+                            listType="picture-card"
+                            beforeUpload={() => false}
+                            onChange={handleUploadChange}
+                            maxCount={1}
+                            fileList={formData.image ? [{
+                                uid: '-1',
+                                name: 'image.png',
+                                status: 'done',
+                                url: formData.image,
+                            }] : []}
+                        >
+                            {!formData.image && (
+                            <div>
+                                <PlusOutlined />
+                                <div style={{ marginTop: 8 }}>Thêm ảnh</div>
+                            </div>
+                            )}
+                        </Upload>
+                    </div>
+
+                    {/* Nút Preview */}
+                    <Button
+                        type="primary"
+                        onClick={() => {
+                            if (!formData.title.trim()) {
+                                message.warning("Vui lòng nhập Title!");
+                                return;
+                            }
+                            if (!formData.description.trim()) {
+                                message.warning("Vui lòng nhập Description!");
+                                return;
+                            }
+                            if (!formData.image) {
+                                message.warning("Vui lòng thêm Ảnh!");
+                                return;
+                            }
+                            setPreviewOpen(true);
+                        }}
+                    >
+                        Xem Preview
+                    </Button>
+
+                    {/* MODAL PREVIEW */}
+                    <Modal
+                        title="Xem trước nội dung"
+                        open={previewOpen}
+                        onCancel={() => setPreviewOpen(false)}
+                        footer={[
+                            <Button key="cancel" onClick={() => setPreviewOpen(false)}>Hủy</Button>,
+                            <Button key="confirm" type="primary" loading={loading} onClick={handleConfirm}>Xác nhận lưu</Button>
+                        ]}
+                    >
+                        <div className="w-full bg-white rounded overflow-hidden shadow-lg max-w-sm mx-auto hover:shadow-xl transition-shadow duration-300">
+                            <div className="relative w-full h-64 md:h-72 lg:h-80 overflow-hidden">
+                                {/* Ảnh nền */}
+                                <div
+                                    style={{ backgroundImage: `url(${formData.image})` }}
+                                    className="bg-cover bg-center w-full h-full transform transition-transform duration-500 hover:scale-105"
+                                ></div>
+
+                                {/* Overlay gradient đen mờ dần từ trên xuống dưới */}
+                                <div className="absolute inset-0 bg-gradient-to-b from-black/15 via-transparent to-black/70 pointer-events-none"></div>
+                            </div>
+
+                            <div className='p-6'>
+                                <h2 className="text-xl md:text-2xl font-semibold text-gray-900 mb-2 relative z-10">{formData.title}</h2>
+                                <p className='text-sm text-gray-500'>{formData.description}</p>
+                            </div>
+
+                        </div>
+                    </Modal>
+                </div>
+            </div>
+
         </div>
     )
 }
