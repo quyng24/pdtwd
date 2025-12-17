1350e99211679388f81ca1a0d1203a9062cc1268
Nguyen Van Quy <nquy50771@gamil.com>
2025-12-17 22:49:27 +0700

    add component camera capture

--- app/components/RegisterStudent.tsx
+++ app/components/RegisterStudent.tsx
@@ -1,33 +1,33 @@
-import { useState } from "react";
+"use client";
+
+import { Input } from "antd";
 import CameraCapture from "./CameraCapture";
+import { useState } from "react";
 
 export default function RegisterStudent() {
-  const [nameStudent, setNameStudent] = useState<string>();
-  const [imgBase64, setImgBase64] = useState("");
+  const [nameStudent, setNameUser] = useState<string>("");
 
-  const submit = () => {
-    if (!imgBase64) return alert("Chưa chụp ảnh!");
-    console.log(nameStudent, imgBase64);
-    alert("Capture success!");
+  const handleFaceCaptured = (base64: string) => {
+    // Ở đây bạn tự do xử lý: call API, lưu state, hiển thị ảnh, v.v.
+    if (base64) {
+      console.log({ name: nameStudent, image_base64: base64 });
+      setNameUser("");
+    }
   };
+
   return (
-    <div>
-      <h2 className="text-xl font-semibold mb-3">Thêm học sinh mới</h2>
-
-      <input
-        className="border p-2 rounded mb-4"
-        placeholder="Tên học sinh"
-        onChange={(e) => setNameStudent(e.target.value)}
+    <div className="flex flex-col items-center gap-4">
+      <Input
+        placeholder="Nhập họ tên học sinh"
+        value={nameStudent}
+        onChange={(e) => setNameUser(e.target.value)}
+        type="text"
       />
-
-      <CameraCapture onCapture={(img) => setImgBase64(img)} />
-
-      <button
-        onClick={submit}
-        className="mt-4 px-4 py-2 bg-green-600 text-white rounded"
-      >
-        Lưu học sinh
-      </button>
+      <CameraCapture
+        onFaceCaptured={handleFaceCaptured}
+        showPreview={true}
+        autoCloseAfterCapture={true}
+      />
     </div>
   );
 }
--- app/components/TakeAttendance.tsx
+++ app/components/TakeAttendance.tsx
@@ -1,29 +1,15 @@
 "use client";
 
 import CameraCapture from "./CameraCapture";
-import { useState } from "react";
 
 export default function TakeAttendance() {
-  const [imgBase64, setImgBase64] = useState<string | null>(null);
-  const [result, setResult] = useState<string>("");
-
-  const check = () => {
-    if (!imgBase64) return alert("Chưa chụp ảnh!");
-    console.log(imgBase64);
-    alert("Capture success!");
-  };
+  const handleFaceCaptured = (base64: string) => {};
   return (
-    <div>
-      <h2 className="text-xl font-semibold mb-3">Điểm danh</h2>
-      <CameraCapture onCapture={(img) => setImgBase64(img)} />
-      <button
-        onClick={check}
-        className="mt-4 px-4 py-2 bg-purple-600 text-white rounded"
-      >
-        Xác thực điểm danh
-      </button>
-
-      {result && <p className="mt-3 text-lg font-bold">{result}</p>}
-    </div>
+    <CameraCapture
+      onFaceCaptured={handleFaceCaptured}
+      showPreview={true}
+      buttonTextOff="Bắt đầu điểm danh"
+      buttonTextOn="Kết thúc điểm danh"
+    />
   );
 }
