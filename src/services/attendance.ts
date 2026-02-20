569bfc0bb2b8a9bcd50e9d3f9db59f23eb8875c7
Nguyen Van Quy <nquy50771@gamil.com>
2026-02-20 10:13:48 +0700

    redeploy config api router

---
+++ src/services/attendance.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { AttendancePayload } from "@/types/type"
+
+const ATTENDANCE_API = `${process.env.NEXT_PUBLIC_API_BASE}/api/attendance`
+
+export const attendanceStudentApi = async (data: AttendancePayload) => {
+  try {
+    const response = await axios.post(
+      `${ATTENDANCE_API}/main`,
+      data
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export const attendanceLogApi = async (page: number = 1, page_size: number = 5) => {
+  try {
+    const response = await axios.get(`${ATTENDANCE_API}/logs?page=${page}&page_size=${page_size}`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
--- src/services/student.ts
+++ src/services/student.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { AttendancePayload, StudentsType } from "../../src/types/type";
-const STUDENT_API = `${process.env.NEXT_PUBLIC_API_BASE}/students`;
+const STUDENT_API = `${process.env.NEXT_PUBLIC_API_BASE}/api/students`;
 
 export const createStudentApi = async (data: StudentsType) => {
   try {
@@ -15,25 +15,3 @@ export const createStudentApi = async (data: StudentsType) => {
   }
 };
 
-export const attendanceStudentApi = async (data: AttendancePayload) => {
-  try {
-    const response = await axios.post(
-      `${STUDENT_API}/attendance`,
-      data
-    );
-    return response.data;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
-};
-
-export const attendanceLogApi = async (page: number = 1, page_size: number = 5) => {
-  try {
-    const response = await axios.get(`${STUDENT_API}/attendance/logs?page=${page}&page_size=${page_size}`);
-    return response.data;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
-}
